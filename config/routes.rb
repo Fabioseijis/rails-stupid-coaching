5798c748df81b1bcc636c83383e1ca943de2287f
Fabio Seiji dos Santos <fabio1987_@hotmail.com>
2020-02-17 15:49:55 -0300

    complete, no css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'questions#ask', as: :ask
+  get '/answer', to: 'questions#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ask GET /ask(.:format) questions#ask
+  # answer GET /answer(.:format) questions#anwer
 end
